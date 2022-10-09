07383379bf6b6d1d76cbfd5355202bf7b0a6469e
Faisal <faisal.ashfaq047@gmail.com>
2022-10-09 22:02:44 +0500

    1st JavaScript Hello World assingmnet

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("Hello User");
+window.alert("how are you");
+document.write("Hey Bro - are you saying");
+
+var age = 24;
+var nationality = "pakistani";
+
+console.log("variable add");
+
+var me;
+me = 34;
+console.log("age added");
+
